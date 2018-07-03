0330d04b8ade4f4304937217f9eb741bb45ccd9b
xueliang <1058392650@qq.com>
2018-07-03 11:47:46 +0800

    '修改vue.config'

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,38 @@
+const path = require('path')
+const debug = process.env.NODE_ENV !== 'production'
+
 module.exports = {
-    outputDir:"dist",
+    outputDir: "dist",
     devServer: {
-        port:8990,
-        hotOnly: true
-        // proxy: {
-        //     '/api':{
-        //         target: 'http://163.music.com',
-        //         ws: true,
-        //         changeOrigin: true,
-        //         pathRewrite: {
-        //             '^/api':""
-        //         }
-        //     }
-        // }
+        open: true,
+        port: 8990,
+        hotOnly: true,
+        proxy: {
+            '/api': {
+                target: 'http://163.music.com',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ""
+                }
+            }
+        }
     },
-    css:{
+    // configureWebpack: config => { // webpack配置，值位对象时会合并配置，为方法时会改写配置
+    //     if (debug) { // 开发环境配置
+    //         config.devtool = 'cheap-module-eval-source-map'
+    //     } else { // 生产环境配置
+    //     }
+    //     Object.assign(config, { // 开发生产共同配置
+    //         resolve: {
+    //             alias: {
+    //                 '@': path.resolve(__dirname, './src'),
+    //                 'vue$': 'vue/dist/vue.esm.js'
+    //             }
+    //         }
+    //     })
+    // },
+    css: {
         loaderOptions: {
             css: {
                 // options here will be passed to css-loader
