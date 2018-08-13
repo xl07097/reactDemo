a3fc50e349ce1811773707f7c6e5a5581df8bce3
xueliang <1058392650@qq.com>
2018-08-13 09:10:36 +0800

    路径

--- src/views/personal/index.vue
+++ src/views/personal/index.vue
@@ -4,31 +4,28 @@
     </div>
 </template>
 <script>
-import axios from 'axios'
-import Cookies from 'js-cookie'
+import axios from "axios";
+import Cookies from "js-cookie";
 export default {
-    name:"person",
-    data(){
-        return {
-            
-        }
-    },
-    methods:{
-        getInfo(){
-            axios.get("http://192.168.1.49:3000/api/user/info").then(function(data){
-                console.log(data);
-            })
-        }
-    },
-    created(){
-        console.dir(Cookies)
-        this.getInfo();
+  name: "person",
+  data() {
+    return {};
+  },
+  methods: {
+    getInfo() {
+      axios.get("http://192.168.1.49:3000/api/user/info").then(function(data) {
+        console.log(data);
+      });
     }
-}
+  },
+  created() {
+    console.dir(Cookies);
+    this.getInfo();
+  }
+};
 </script>
 
 <style scoped lang="less">
-
 </style>
 
 
