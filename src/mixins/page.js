593d4f4013013005f4555378a8329c2c41eb3bbb
xueliang <1058392650@qq.com>
2018-09-25 11:31:24 +0800

    添加混合mixins

---
+++ src/mixins/page.js
@@ -0,0 +1,12 @@
+export default {
+    data() {
+        return {
+            pageConfig: {
+                page: 1,
+                size: 10,
+                total: 0,
+                opts: [10, 20, 50, 100]
+            }
+        }
+    }
+}
