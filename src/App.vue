2d2a84b9223da2099b99be1852b7b9916fc493cf
xueliang <1058392650@qq.com>
2018-12-20 21:47:43 +0800

    'format'

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,26 @@
 <template>
-  <div id="app">
-    <Header></Header>
-    <router-view/>
-    <Footer></Footer>
-  </div>
+    <div id="app">
+        <Header></Header>
+        <router-view/>
+        <Footer></Footer>
+    </div>
 </template>
 <script>
-import Header from "@/components/Header"
-import Footer from "@/components/Footer"
-export default {
-  components:{
-    Header,
-    Footer
-  }
-}
+    import Header from "@/components/Header"
+    import Footer from "@/components/Footer"
+
+    export default {
+        components: {
+            Header,
+            Footer
+        }
+    }
 </script>
 
 <style lang="less">
-#app {
-  padding: 60px 0;
-  color: #2c3e50; 
-}
+    #app {
+        padding: 60px 0;
+        color: #2c3e50;
+    }
 
 </style>
