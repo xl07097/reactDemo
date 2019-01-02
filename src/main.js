1f9b8f368f8f1590f73f47df064fe0420b5a82ef
xueliang <1058392650@qq.com>
2019-01-02 09:44:16 +0800

    aha

--- src/main.js
+++ src/main.js
@@ -1,23 +1,16 @@
 import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router/router'
-// import store from '@/store/store'
-// import Alert from '@/components/alert/index'
-
 import iView from 'iview'
-
 import 'iview/dist/styles/iview.css';
-// import 'font-awesome/css/font-awesome.css'
 import './common.less'
 
 Vue.use(iView)
-// Vue.use(Alert)
 
 Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
   router,
-  // store,
   render: h => h(App)
 }).$mount('#app')
