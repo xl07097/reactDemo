822d5dcb55cc2ea642535a4d9eb68458f77ee4ae
xueliang <1058392650@qq.com>
2019-01-03 12:12:49 +0800

    host

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ module.exports = {
     baseUrl: './',
     outputDir: "dist",
     devServer: {
-        host: 'http://192.168.1.49',
         open: true,
         port: 9001,
         hotOnly: true
