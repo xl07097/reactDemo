2f53356c55a4fc3833f729977594f835bd3ffcfd
xueliang <xueliang@szusoft.com>
2019-02-28 15:50:24 +0800

    项目内容重置

--- src/store/systemConfig.js
+++
@@ -1,9 +0,0 @@
-const state = {
-    state: {
-        appId:'1234567890',
-        aesKey:'abcdefg123456789',
-        ivKey:'abcdefg123456789',
-        appKey:'abcdefg1234567890',
-    }
-}
-export default state
