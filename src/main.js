8284079ab1ffa847062c99690616a1bf260443f7
xueliang <xueliang@szusoft.com>
2019-03-09 14:11:03 +0800

    moment

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import 'antd/dist/antd.css';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+
+moment.locale('zh-cn');
+
 import App from './views/app';
 
 
