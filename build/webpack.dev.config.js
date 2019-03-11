c41ccd5cfb0cae1c81ff44895fa9909c476b5c29
xueliang <xueliang@szusoft.com>
2019-03-11 10:09:23 +0800

    format

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -2,48 +2,54 @@ const path = require('path');
 const merge = require('webpack-merge');
 const base = require('./webpack.base.config');
 
-module.exports = merge(base,{
+module.exports = merge(base, {
     mode: 'development',
     devtool: 'cheap-module-eval-source-map',
     output: {
-        path: path.resolve(__dirname,'../dist'),
+        path: path.resolve(__dirname, '../dist'),
         filename: 'js/[name].js',
         publicPath: '/'
     },
-    module:{
-        rules:[
-            {
+    module: {
+        rules: [{
                 test: /\.css$/,
-                use:[{
-                    loader: 'style-loader',
-                },{
-                    loader: 'css-loader',
-                },{
-                    loader: 'postcss-loader',
-                }]
+                use: [{
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader',
+                    }
+                ]
             },
             {
                 test: /\.less$/,
-                use:[{
-                    loader: 'style-loader',
-                },{
-                    loader: 'css-loader',
-                },{
-                    loader: 'postcss-loader',
-                },{
-                    loader: 'less-loader',
-                    options:{
-                        javascriptEnabled: true
+                use: [{
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader',
+                    },
+                    {
+                        loader: 'less-loader',
+                        options: {
+                            javascriptEnabled: true
+                        }
                     }
-                }]
+                ]
             },
             {
                 test: /\.(scss|sass)$/,
-                use:['style-loader', 'css-loader','postcss-loader','sass-loader']
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
             }
         ]
     },
-    devServer:{
+    devServer: {
         port: '9000',
         open: true,
         hot: true
