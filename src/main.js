2ac24c4c74c1119e7549260bcd1527546828ce0f
xueliang <xueliang@szusoft.com>
2019-05-09 17:37:43 +0800

    数组去重

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import zhCN from 'antd/lib/locale-provider/zh_CN';
 import moment from 'moment';
 import 'moment/locale/zh-cn';
 
+import "./common.less";
+
 moment.locale('zh-cn');
 
 import App from './views/app';
--- src/views/content/Content.jsx
+++ src/views/content/Content.jsx
@@ -17,6 +17,17 @@ class Content extends React.Component {
     }
     componentDidMount(){
         console.log(this.props)
+        let arr = [12,12,13,2,6,5,2,3,6,56,23,126,2,6,5,2];
+
+        console.log(Array.from(new Set([...arr])));
+
+        let tObj = {};
+
+        arr.forEach((item,index) => {
+            tObj[item] = item;
+        })
+
+        console.log(Object.values(tObj));  // 改变数组顺序
     }
 
     cancel = () => {
