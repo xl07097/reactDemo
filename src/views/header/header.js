5ae5d472f0c9aecb253ec68ec2153fe7fb09d4d6
xueliang <xueliang@szusoft.com>
2019-05-27 15:10:18 +0800

    结构

--- src/views/header/header.js
+++ src/views/header/header.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import {Link, IndexLink} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import './header.less'
 
-class Header extends React.Component {
-    
-    render() {
-        return (
+
+function Header(){
+    return (
         <header className="header">
             <div className="header-left">
                 <h1 className="header-title">创客</h1>
-                    <Link to="/">index</Link>
+                <Link to="/">index</Link>
                 <Link to='/chart'>chart</Link>
                 <Link to='/other'>other</Link>
                 <Link to='/other/chart'>link4</Link>
             </div>
-           
+
         </header>
-        )
-    }
+    )
 }
 
 export default Header
