29b445a226f6e4250bbfb6fa498db6c147f7397e
xueliang <xueliang@szusoft.com>
2019-05-27 16:03:20 +0800

    eslint

--- src/views/content/Content.jsx
+++ src/views/content/Content.jsx
@@ -15,14 +15,6 @@ class Content extends React.Component {
         let arr = [12, 12, 13, 2, 6, 5, 2, 3, 6, 56, 23, 126, 2, 6, 5, 2];
 
         console.log(Array.from(new Set([...arr])));
-
-        let tObj = {};
-
-        arr.forEach((item, index) => {
-            tObj[item] = item;
-        })
-
-        console.log(Object.values(tObj));  // 改变数组顺序
     }
 
     render() {
