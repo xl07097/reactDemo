8156cd6371149cfb4cd5144382e5dc7161d89e8c
xueliang <xueliang@szusoft.com>
2019-07-18 16:57:03 +0800

    bread

--- src/components/bread/breadcrumb.js
+++ src/components/bread/breadcrumb.js
@@ -2,58 +2,66 @@ import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import { Breadcrumb } from 'antd';
 
-import { asynRouter } from '@/router/router'
+import { asynRouter } from '@/router/router';
+let menu = {};
 
-
-// class MyBread extends React.Component {
-
-// }
-
-
-function itemRender(route, params, routes, paths) {
-    console.log(route);
-    // console.log(params);
-    // console.log(routes);
-    // console.log(paths);
-
-    const last = routes.indexOf(route) === routes.length - 1;
-    return last ? (
-        <span>{route.meta.title}</span>
-    ) : (
-            <Link to={paths.join('/')}>{route.meta.title}</Link>
-        );
+function po(router, path){
+    router.forEach(element => {
+        let key = '';
+        if(element.path){
+            key = `${path}/${element.path}`;
+            key = key.replace(/\/+/, '/');
+            menu[key] = element.meta.title;
+        }
+        let children = element.children || [];
+        po(children, key);
+    });
 }
-
+po(asynRouter, '');
 
 function MyBread(props) {
 
-    // const { location } = props;
-    // const locationArr = location.pathname.split('/').filter(i => i);
+    // function itemRender(route, params, routes, paths) {
+    //     console.log(route);
+    //     // console.log(params);
+    //     // console.log(routes);
+    //     // console.log(paths);
+    //     // console.log(routes.indexOf(route))
+    //     const last = routes.indexOf(route) === routes.length - 1;
+    //     return last ? (
+    //         <span>{route.meta.title}</span>
+    //     ) : (
+    //             <Link to={paths.join('/')}>{route.meta.title}</Link>
+    //         );
+    // }
 
-    // let breadcrumbItems = locationArr.map((item, index, arr) => {
-    //     let key = arr.slice(0, index + 1).join('/');
-    //     return (
-    //         <Breadcrumb.Item key={key}>
-    //             <Link to={`/${key}`}>{item}</Link>
-    //         </Breadcrumb.Item>
-    //     )
-    // })
-    // breadcrumbItems.unshift((
-    //     <Breadcrumb.Item key="home">
-    //         <Link to="/">Home</Link>
-    //     </Breadcrumb.Item>
-    // ))
+    const { location } = props;
+    const locationArr = location.pathname.split('/').filter(i => i);
+
+    let breadcrumbItems = locationArr.map((item, index, arr) => {
+        let key = arr.slice(0, index + 1).join('/');
+        return (
+            <Breadcrumb.Item key={key}>
+                <Link to={`/${key}`}>{menu[`/${key}`]}</Link>
+            </Breadcrumb.Item>
+        )
+    })
+    breadcrumbItems.unshift((
+        <Breadcrumb.Item key="home">
+            <Link to="/">Home</Link>
+        </Breadcrumb.Item>
+    ))
 
 
-    // return (
-    //     <div style={{ padding: '8px 6px',marginBottom:'8px', background: '#FFFFFF' }}>
-    //         <Breadcrumb>
-    //             {breadcrumbItems}
-    //         </Breadcrumb>
-    //     </div>
-    // )
+    return (
+        <div style={{ padding: '8px 6px',marginBottom:'8px', background: '#FFFFFF' }}>
+            <Breadcrumb>
+                {breadcrumbItems}
+            </Breadcrumb>
+        </div>
+    )
 
-    return <Breadcrumb itemRender={itemRender} routes={asynRouter} />;
+    // return <Breadcrumb itemRender={itemRender} routes={asynRouter} />;
 
 }
 
