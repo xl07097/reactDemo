598a8cbb420f326448fce1f67aa37e0ece690ef3
xueliang <xueliang@szusoft.com>
2019-08-08 17:44:01 +0800

    redux

--- src/model/eg1.js
+++ src/model/eg1.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 
 function visibilityFilter(state = 'SHOW_ALL', action) {
     if (action.type === 'SET_VISIBILITY_FILTER') {
@@ -23,9 +25,11 @@ function todos(state = [], action) {
     }
 }
 
-function todoApp(state = {}, action) {
+function todoApp(state = {}, action) {  // 自定义
     return { 
         visibilityFilter: visibilityFilter(state.visibilityFilter, action),
         todos: todos(state.todos, action)
     };
 }
+
+let reducer = combineReducers({visibilityFilter, todos}); // 使用 combineReducers 函数合并 reducer 
