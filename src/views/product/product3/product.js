8300dcb9c21e733dc65740d3e4c9724a13b682c2
xueliang <xueliang@szusoft.com>
2019-08-08 15:57:21 +0800

    useEffect 销毁

--- src/views/product/product3/product.js
+++ src/views/product/product3/product.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect }from "react";
 
 function Product() {
+    let timer = null;
+    useEffect(() => {
+        timer = setInterval(() => {
+            fetch('http://localhost:9087/note/socket/push/1?message=hello')
+                .then(data => data.text())
+                .then(data => {
+                    console.log(data);
+                });
+        }, 2000);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
     return (<h2>product3</h2>);
 }
 
