7db037c971cf0134473284e427350d9c9aedd48c
xueliang <xueliang@szusoft.com>
2019-09-16 14:54:20 +0800

    add treeUtils.ts

---
+++ src/utils/treeUtils.ts
@@ -0,0 +1,26 @@
+function array2Tree(data:any) {
+    let zNodes = data;
+    let tmpMap = {};
+    let tree:any = [];
+    let key = 'id';
+    let parentKey = 'pid';
+    let childKey = 'children';
+
+    zNodes.forEach((item: any) => {
+        tmpMap[item[key]] = item;
+    });
+
+    zNodes.forEach((item:any) => {
+        if (tmpMap[item[parentKey]] && item[key] !== item[parentKey]) {
+            if (!tmpMap[item[parentKey]][childKey])
+                tmpMap[item[parentKey]][childKey] = [];
+            tmpMap[item[parentKey]][childKey].push(item);
+        } else {
+            tree.push(item);
+        }
+    })
+
+    return tree;
+}
+
+export default array2Tree;
--- src/views/product/product4/product.jsx
+++ src/views/product/product4/product.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Tree } from 'antd';
 import { getAllMenu } from '@/api/menu';
 
-// import { getUserList } from '@/api/product'
+import array2Tree from '@/utils/treeUtils';
 
 
 function Product(props) {
@@ -16,27 +16,7 @@ function Product(props) {
         getAllMenu()
             .then(data => {
                 if (data.code === 200) {
-                    let zNodes = data.data;
-                    let tmpMap = {};
-                    let r = [];
-                    let key = 'id';
-                    let parentKey = 'pid';
-                    let childKey = 'children';
-
-                    for (let i = 0, l = zNodes.length; i < l; i++) {
-                        tmpMap[zNodes[i][key]] = zNodes[i];
-                    }
-
-                    for (let i = 0, l = zNodes.length; i < l; i++) {
-                        if (tmpMap[zNodes[i][parentKey]] && zNodes[i][key] !== zNodes[i][parentKey]) {
-                            if (!tmpMap[zNodes[i][parentKey]][childKey])
-                                tmpMap[zNodes[i][parentKey]][childKey] = [];
-                            tmpMap[zNodes[i][parentKey]][childKey].push(zNodes[i]);
-                        } else {
-                            r.push(zNodes[i]);
-                        }
-                    }
-                    setTreeData(r)
+                    setTreeData(array2Tree(data.data));
                 }
             })
     }
