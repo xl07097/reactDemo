8d142a3be8f4130a91b17ef0be6883578e1546c4
xueliang <xueliang@szusoft.com>
2019-09-17 17:48:15 +0800

    col

--- src/views/tree/index.jsx
+++ src/views/tree/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Row, Col } from 'antd';
 
 import Tree1 from './Tree';
 
 function Index() {
     return (
-        <>
+        <Row>
+            <Col span={6}>
+
+            </Col>
             <Tree1></Tree1>
-        </>
+        </Row>
     )    
 }
 
