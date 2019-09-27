2fabaf2c09c8a2137eb92c6b471ae8bb48006ef6
xueliang <xueliang@szusoft.com>
2019-09-27 09:59:18 +0800

    文件

---
+++ src/views/button/Index.jsx
@@ -0,0 +1,26 @@
+import React, { lazy, Suspense } from "react";
+import { Row, Col } from 'antd';
+
+import Button1 from "./Button1";
+import Button2 from "./Button2";
+import Button3 from "./Button3";
+
+function RenderButtonGroup() {
+    return (
+        <div>
+            <Row gutter={24}>
+                <Col span={8}>
+                    <Button1 />
+                </Col>
+                <Col span={8}>
+                    <Button2 />
+                </Col>
+                <Col span={8}>
+                    <Button3 />
+                </Col>
+            </Row>
+        </div>
+    );
+}
+
+export default RenderButtonGroup;
