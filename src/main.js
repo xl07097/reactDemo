0c4da445e7ea1239646532c5b46d318a6b232630
xueliang <xueliang@szusoft.com>
2019-09-27 10:51:29 +0800

    自定义 color

--- src/main.js
+++ src/main.js
@@ -5,17 +5,16 @@ import { Provider } from 'react-redux';
 import store from '@/model/store';
 import { BrowserRouter } from "react-router-dom";
 import { ConfigProvider  } from "antd";
-
-import zh_CN from "antd/lib/locale-provider/zh_CN";
-import moment from "moment";
-
-import "moment/locale/zh-cn";
-
-import "./common.less";
-
 import App from "./views/App";
 
+import zh_CN from "antd/lib/locale-provider/zh_CN";
+
+import moment from "moment";
+import "moment/locale/zh-cn";
 moment.locale("zh-cn");
+
+
+import "./common.less";
 
 ReactDOM.render(
     <Provider store={store}>
