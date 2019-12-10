86c25b34d1a1ab39e1b83391b2e7a10ab1c226ae
xueliang <1058392650@qq.com>
2019-12-10 16:37:35 +0800

    icon

--- src/views/icons/Icon.jsx
+++ src/views/icons/Icon.jsx
@@ -8,7 +8,7 @@ function RenderIcon() {
     let arr = Array.from(new Set(emoji["font-awesome"]));
 
     return (
-        <div style={{background:"rgba(256,256,256,0.4)",textAlign:"justify",minHeight: "calc(100%)"}}>
+        <div style={{'background':"rgba(256,256,256,0.4)",'textAlign':"justify",'minHeight': "calc(100%)"}}>
             {arr.map(item => {
                 let key = `${item}${~~(Math.random()*1000000)}`;
                 return <i key={key} className={`fa fa-${item} fa-3x item`} aria-hidden="true"></i>;
