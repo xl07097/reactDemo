9d2ffaa0e2c58e21fe435d92226d76df9c664451
xueliang <1058392650@qq.com>
2020-05-14 17:01:51 +0800

    包

--- src/components/sidemenu/side_1.jsx
+++ src/components/sidemenu/side_1.jsx
@@ -6,97 +6,101 @@ import { Menu } from "antd";
 import { asynRouter } from "@/router/router";
 
 const style = {
-  width: 234,
+    width: 234,
 };
 
 function getOpenkeys(key) {
-  let openKeys = key.split("/").map((item, index, arr) => {
-    return arr.slice(0, index + 1).join("/");
-  });
-  openKeys.shift();
-  return openKeys;
+    let openKeys = key.split("/").map((item, index, arr) => {
+        return arr.slice(0, index + 1).join("/");
+    });
+    openKeys.shift();
+    return openKeys;
 }
 function Side(props) {
-  const { location } = props;
+    const { location } = props;
 
-  const defaultSelectedKeys = [location.pathname];
+    const defaultSelectedKeys = [location.pathname];
 
-  let openKey = getOpenkeys(location.pathname);
+    let openKey = getOpenkeys(location.pathname);
 
-  let defaultOpenKeys = openKey;
-  let [openKeys, setOpenKeys] = useState(openKey);
-  // let openKeys = getOpenkeys(location.pathname)
+    let defaultOpenKeys = openKey;
+    let [openKeys, setOpenKeys] = useState(openKey);
+    // let openKeys = getOpenkeys(location.pathname)
 
-  // let [selectKeys, setSelectKeys] = useState([location.pathname])
-  let selectKeys = [location.pathname];
+    // let [selectKeys, setSelectKeys] = useState([location.pathname])
+    let selectKeys = [location.pathname];
 
-  function titleClick({ key }) {
-    let openKeys = getOpenkeys(key);
-    setOpenKeys(openKeys);
-  }
+    function titleClick({ key }) {
+        let openKeys = getOpenkeys(key);
+        setOpenKeys(openKeys);
+    }
 
-  function renderSubMenu(route, parentRoute) {
-    return route.map((item) => {
-      if (item.path === "*") {
-        return null;
-      }
-      let newPath;
-      if (/^\//.test(item.path)) {
-        newPath = `${item.path}`;
-      } else {
-        newPath = `${parentRoute}/${item.path}`;
-      }
-      newPath = newPath.replace(/\/+/g, "/");
-      if (item.children) {
-        return (
-          <Menu.SubMenu
-            path={newPath}
-            key={newPath}
-            title={item.meta.title}
-            onTitleClick={titleClick}
-          >
-            {item.children && renderSubMenu(item.children, newPath)}
-          </Menu.SubMenu>
-        );
-      } else {
-        return (
-          <Menu.Item path={newPath} key={newPath}>
-            <Link to={newPath}>{item.meta.title}</Link>
-          </Menu.Item>
-        );
-      }
-    });
-  }
-  function openChange(openKeys) {
-    setOpenKeys(openKeys);
-  }
+    function renderSubMenu(route, parentRoute) {
+        return route.map((item) => {
+            if (item.path === "*") {
+                return null;
+            }
+            let newPath;
+            if (/^\//.test(item.path)) {
+                newPath = `${item.path}`;
+            } else {
+                newPath = `${parentRoute}/${item.path}`;
+            }
+            newPath = newPath.replace(/\/+/g, "/");
+            if (item.children) {
+                return (
+                    <Menu.SubMenu
+                        path={newPath}
+                        key={newPath}
+                        title={
+                            <span>
+                                <Icon type="mail" />
+                                <span>{item.meta.title}</span>
+                            </span>
+                        }
+                        onTitleClick={titleClick}
+                    >
+                        {item.children && renderSubMenu(item.children, newPath)}
+                    </Menu.SubMenu>
+                );
+            } else {
+                return (
+                    <Menu.Item path={newPath} key={newPath}>
+                        <Link to={newPath}>
+                            <Icon type="mail" />
+                            <span>{item.meta.title}</span>
+                        </Link>
+                    </Menu.Item>
+                );
+            }
+        });
+    }
 
-  function clickMenu({ item, key, keyPath, domEvent }) {
-    setOpenKeys(keyPath.reverse());
-    console.log(keyPath);
-  }
+    function clickMenu({ item, key, keyPath, domEvent }) {
+        setOpenKeys(keyPath.reverse());
+        console.log(keyPath);
+    }
 
-  const openKeyProps = props.collapse ? {} : { openKeys };
-  console.log(openKeyProps);
+    const openKeyProps = props.collapse ? {} : { openKeys };
 
-  return (
-    <Menu
-      theme="dark"
-      mode="inline"
-      {...openKeyProps}
-      // onOpenChange={openChange}
-      // defaultSelectedKeys={defaultSelectedKeys}
-      defaultOpenKeys={defaultOpenKeys}
-      selectedKeys={selectKeys}
-      // openKeys={openKeys}
-      onClick={clickMenu}
-    >
-      {renderSubMenu(asynRouter, "/")}
-    </Menu>
-  );
+    return (
+        <Menu
+            theme="dark"
+            mode="inline"
+            {...openKeyProps}
+            // onOpenChange={openChange}
+            // defaultSelectedKeys={defaultSelectedKeys}
+            defaultOpenKeys={defaultOpenKeys}
+            selectedKeys={selectKeys}
+            // openKeys={openKeys}
+            onClick={clickMenu}
+        >
+            {renderSubMenu(asynRouter, "/")}
+        </Menu>
+    );
 }
 export default connect((state) => ({
-  collapse: state.collapse,
-  mode: state.collapse ? "vertical" : "inline",
+    collapse: state.collapse,
+    mode: state.collapse ? "vertical" : "inline",
 }))(withRouter(Side));
 // export default withRouter(Side);
