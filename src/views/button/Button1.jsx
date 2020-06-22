9100c9ad3f0f466144bb8872f4a66518bf5215f7
xueliang <xueliang@szusoft.com>
2020-06-22 16:10:56 +0800

    remove restting

--- src/views/button/Button1.jsx
+++ src/views/button/Button1.jsx
@@ -3,12 +3,12 @@ import { Button } from "antd";
 
 function RenderButton() {
     return (
-        <div style={{padding:"10px 0",textAlign:"center",background:'#ccc'}}>
-            <Button type="primary">primary</Button>
-            <Button>默认</Button>
-            <Button type="dashed">虚线</Button>
-            <Button type="danger">危险</Button>
-            <Button type="link">链接</Button>
+        <div style={{ padding: "10px 0", textAlign: "center", background: "#ccc" }}>
+            <Button type="primary">primary</Button>&emsp;
+            <Button>默认</Button>&emsp;
+            <Button type="dashed">虚线</Button>&emsp;
+            <Button type="danger">危险</Button>&emsp;
+            <Button type="link">链接</Button>&emsp;
             <Button ghost>幽灵</Button>
         </div>
     );
