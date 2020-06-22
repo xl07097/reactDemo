be5eba2e4da6389318735cd15a8912a1108d7c6d
xueliang <xueliang@szusoft.com>
2020-06-22 15:26:55 +0800

    g

--- src/views/button/Button1.jsx
+++ src/views/button/Button1.jsx
@@ -3,13 +3,13 @@ import { Button } from "antd";
 
 function RenderButton() {
     return (
-        <div style={{textAlign:"center"}}>
+        <div style={{padding:"10px 0",textAlign:"center",background:'#ccc'}}>
             <Button type="primary">primary</Button>
             <Button>默认</Button>
             <Button type="dashed">虚线</Button>
             <Button type="danger">危险</Button>
             <Button type="link">链接</Button>
-            <Button type="ghost">幽灵</Button>
+            <Button ghost>幽灵</Button>
         </div>
     );
 }
