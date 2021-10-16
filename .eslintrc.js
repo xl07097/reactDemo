d3c6b9dee0886c6884217ee75b28936662690e81
xueliang <1058392650@qq.com>
2021-10-16 22:42:07 +0800

    格式化优化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+        browser: true,
+        es6: true,
     },
-    "extends": ["airbnb", "plugin:prettie/recommended"],
+    extends: ["airbnb", "plugin:prettier/recommended"],
     // "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
     },
-    "parser": 'babel-eslint',
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    parser: "babel-eslint",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
         },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+        ecmaVersion: 2018,
+        sourceType: "module",
     },
-    "plugins": [
-        "import",
-        "react",
-        "react-hooks"
-    ],
-    "rules": {
-        // "indent": ["error", 4],
-        "semi": [0, "always"],
-        "quotes": ["off", "double"],
+    plugins: ["import", "react", "react-hooks"],
+    rules: {
+        indent: ["error", 2],
+        semi: [0, "always"],
+        quotes: ["off", "double"],
         "no-unused-vars": 0,
         "no-console": ["off"],
         "no-undef": ["off"],
-        'indent': 'off'
-    }
+        // 'indent': 'off'
+    },
 };
--- src/views/scroll/Scroll.jsx
+++ src/views/scroll/Scroll.jsx
@@ -1,68 +1,66 @@
-import React from 'react';
-import './style.less';
+import React from "react";
+import "./style.less";
 
-function Scroll(props){
-    return (
-        <div className="scroll-wrapper clearfix">
+function Scroll(props) {
+  return (
+    <div className="scroll-wrapper clearfix">
+      <div className="scrollbar scroll-style-1">
+        <div className="force-overflow"></div>
+      </div>
 
-            <div className="scrollbar scroll-style-1">
-                <div className="force-overflow"></div>
-            </div>
+      <div className="scrollbar scroll-style-2">
+        <div className="force-overflow"></div>
+      </div>
 
-            <div className="scrollbar scroll-style-2">
-                <div className="force-overflow"></div>
-            </div>
+      <div className="scrollbar scroll-style-3">
+        <div className="force-overflow"></div>
+      </div>
 
-            <div className="scrollbar scroll-style-3">
-                <div className="force-overflow"></div>
-            </div>
+      <div className="scrollbar scroll-style-4">
+        <div className="force-overflow"></div>
+      </div>
 
-            <div className="scrollbar scroll-style-4">
-                <div className="force-overflow"></div>
-            </div>
+      <div className="scrollbar scroll-style-5">
+        <div className="force-overflow"></div>
+      </div>
 
-            <div className="scrollbar scroll-style-5">
-                <div className="force-overflow"></div>
-            </div>
+      <div className="scrollbar scroll-style-6">
+        <div className="force-overflow"></div>
+      </div>
 
-            <div className="scrollbar scroll-style-6">
-                <div className="force-overflow"></div>
-            </div>
+      <div className="scrollbar scroll-style-7">
+        <div className="force-overflow"></div>
+      </div>
 
-            <div className="scrollbar scroll-style-7">
-                <div className="force-overflow"></div>
-            </div>
+      <div className="scrollbar scroll-style-10">
+        <div className="force-overflow"></div>
+      </div>
 
+      <div className="scrollbar scroll-style-11">
+        <div className="force-overflow"></div>
+      </div>
 
-            <div className="scrollbar scroll-style-10">
-                <div className="force-overflow"></div>
-            </div>
+      <div className="scrollbar scroll-style-12">
+        <div className="force-overflow"></div>
+      </div>
 
-            <div className="scrollbar scroll-style-11">
-                <div className="force-overflow"></div>
-            </div>
+      <div className="scrollbar scroll-style-13">
+        <div className="force-overflow"></div>
+      </div>
 
-            <div className="scrollbar scroll-style-12">
-                <div className="force-overflow"></div>
-            </div>
+      <div className="scrollbar scroll-style-14">
+        <div className="force-overflow"></div>
+      </div>
 
-            <div className="scrollbar scroll-style-13">
-                <div className="force-overflow"></div>
-            </div>
+      <div className="scrollbar scroll-style-15">
+        <div className="force-overflow"></div>
+      </div>
 
-            <div className="scrollbar scroll-style-14">
-                <div className="force-overflow"></div>
-            </div>
-
-            <div className="scrollbar scroll-style-15">
-                <div className="force-overflow"></div>
-            </div>
-
-            <div className="scrollbar scroll-style-16">
-                <div className="force-overflow"></div>
-            </div>
-        </div>
-    )
+      <div className="scrollbar scroll-style-16">
+        <div className="force-overflow"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Scroll;
