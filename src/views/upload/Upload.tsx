d320bc2d0a7b76852f8a807a80665a6bede78bc2
xueliang <1058392650@qq.com>
2021-10-20 20:52:20 +0800

    Update Upload.tsx

--- src/views/upload/Upload.tsx
+++ src/views/upload/Upload.tsx
@@ -1,44 +1,44 @@
-import React from 'react';
-import { Upload,  message } from 'antd';
-import { InboxOutlined } from '@ant-design/icons';
-import { UploadProps } from 'antd/lib/upload';
-import url from '@/utils/urls';
+import React from "react";
+import { Upload, message } from "antd";
+import { InboxOutlined } from "@ant-design/icons";
+import { UploadProps } from "antd/lib/upload";
+import url from "@/utils/urls";
 
 const { Dragger } = Upload;
 
 const prop: UploadProps = {
-    method: "POST",
-    name: "upfile",
-    multiple: false,
-    headers: {
-        token: sessionStorage.getItem('token') || "",
-    },
-    action: `${url.upload}`,
-    onChange(data) {
-        const { status, response } = data.file;
-        if (status === "uploading") {
-            // console.log(data.file, data.fileList);
-        } else if (status === "done") {
-            message.success(`${data.file.name} file uploaded successfully.`);
-            console.log(response);
-        } else if (status === "error") {
-            message.error(`${data.file.name} file upload failed.`);
-        }
-    },
+  method: "POST",
+  name: "upfile",
+  multiple: false,
+  headers: {
+    token: sessionStorage.getItem("token") || "",
+  },
+  action: `${url.upload}`,
+  onChange(data) {
+    const { status, response } = data.file;
+    if (status === "uploading") {
+      // console.log(data.file, data.fileList);
+    } else if (status === "done") {
+      message.success(`${data.file.name} file uploaded successfully.`);
+      console.log(response);
+    } else if (status === "error") {
+      message.error(`${data.file.name} file upload failed.`);
+    }
+  },
 };
 
 const MyUpload: React.FC<{}> = (props) => {
-    return (
-        <Dragger {...prop}>
-            <p className="ant-upload-drag-icon">
-                <InboxOutlined />
-            </p>
-            <p className="ant-upload-text">Click or drag file to this area to upload</p>
-            <p className="ant-upload-hint">
-                Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files
-            </p>
-        </Dragger>
-    );
+  return (
+    <Dragger {...prop}>
+      <p className="ant-upload-drag-icon">
+        <InboxOutlined />
+      </p>
+      <p className="ant-upload-text">Click or drag file to this area to upload</p>
+      <p className="ant-upload-hint">
+        Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files
+      </p>
+    </Dragger>
+  );
 };
 
 export default MyUpload;
