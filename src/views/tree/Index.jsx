d4693dd4e42f569645635962f436823f9c916b4c
xueliang <1058392650@qq.com>
2022-04-28 13:21:11 +0800

    qs

--- src/views/tree/Index.jsx
+++ src/views/tree/Index.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { Row, Col } from 'antd';
+import React from "react";
+import { Row, Col } from "antd";
 
-import Tree1 from './Tree';
+import Tree1 from "./Tree";
 
 function Index() {
-    return (
-        <Row gutter={24}>
-            <Col span={6}>
+  return (
+    <Row gutter={24}>
+      <Col span={12}>
+        <Tree1></Tree1>
+      </Col>
+      <Col span={12}>
+        <Tree1></Tree1>
+      </Col>
+      {/* <Col span={6}>
                 <Tree1></Tree1>
             </Col>
             <Col span={6}>
                 <Tree1></Tree1>
-            </Col>
-            <Col span={6}>
-                <Tree1></Tree1>
-            </Col>
-            <Col span={6}>
-                <Tree1></Tree1>
-            </Col>
-        </Row>
-    )
+            </Col> */}
+    </Row>
+  );
 }
 
 export default Index;
