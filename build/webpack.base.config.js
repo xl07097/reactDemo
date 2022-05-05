b22a2975f7894df4a21da337d78283f9981d32f2
xueliang <1058392650@qq.com>
2022-05-05 09:50:17 +0800

    format

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -4,130 +4,130 @@ const SpritesmithPlugin = require("webpack-spritesmith");
 const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
 
 module.exports = {
-    entry: {
-        app: path.resolve(__dirname, "../src/main.js"),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                include: path.resolve(__dirname, "../src"),
-                use: [
-                    {
-                        loader: "thread-loader",
-                        options: {
-                            worker: 2,
-                            workerParallelJobs: 30,
-                            poolTimeout: 2000,
-                            poolParallelJobs: 50,
-                        }
-                    },
-                    {
-                        loader: require.resolve("babel-loader"),
-                        options: {
-                            cacheDirectory: true,
-                            cacheCompression: false,
-                            // eslintPath: require.resolve('eslint'),
-                        },
-                    }
-                ],
-
-                exclude: /^node_modules$/,
+  entry: {
+    app: path.resolve(__dirname, "../src/main.js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, "../src"),
+        use: [
+          // {
+          //   loader: "thread-loader",
+          //   options: {
+          //     worker: 2,
+          //     workerParallelJobs: 30,
+          //     poolTimeout: 2000,
+          //     poolParallelJobs: 50,
+          //   }
+          // },
+          {
+            loader: require.resolve("babel-loader"),
+            options: {
+              cacheDirectory: true,
+              cacheCompression: false,
+              // eslintPath: require.resolve('eslint'),
             },
-            {
-                test: /\.(ts|tsx)$/,
-                include: path.resolve(__dirname, "../src"),
-                use: [
-                    'ts-loader'
-                ],
-                exclude: /^node_modules$/,
-            },
-            {
-                test: /\.(png|svg|jpe?g|gif)$/,
-                use: [
-                    {
-                        loader: "url-loader",
-                        options: {
-                            esModule: false,
-                            limit: 10240,
-                            name: path.posix.join("static", "img/[name].[hash:8].[ext]"),
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    {
-                        loader: "url-loader",
-                        options: {
-                            esModule: false,
-                            limit: 10000,
-                            name: path.posix.join("static", "fonts/[name].[hash:8].[ext]"),
-                        },
-                    },
-                ],
-            },
+          }
         ],
-    },
-    plugins: [
-        new AntdDayjsWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: "创客",
-            filename: path.resolve(__dirname, "../dist/index.html"),
-            template: path.resolve(__dirname, "../public/index.html"),
-        }),
-        new SpritesmithPlugin({
-            // 目标小图标
-            src: {
-                //下面的路径，根据自己的实际路径配置
-                cwd: path.resolve(__dirname, "../src/assets/icons"),
-                glob: "*.png",
+
+        exclude: /^node_modules$/,
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        include: path.resolve(__dirname, "../src"),
+        use: [
+          'ts-loader'
+        ],
+        exclude: /^node_modules$/,
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              esModule: false,
+              limit: 10240,
+              name: path.posix.join("static", "img/[name].[hash:8].[ext]"),
             },
-            // 输出雪碧图文件及样式文件
-            target: {
-                //下面的路径，根据自己的实际路径配置
-                image: path.resolve(__dirname, "../public/sprites/sprite.png"),
-                css: path.resolve(__dirname, "../public/sprites/sprite.css"),
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              esModule: false,
+              limit: 10000,
+              name: path.posix.join("static", "fonts/[name].[hash:8].[ext]"),
             },
-            // 样式文件中调用雪碧图地址写法
-            apiOptions: {
-                // 这个路径根据自己页面配置
-                cssImageRef: "./sprite.png",
-            },
-            spritesmithOptions: {
-                algorithm: "top-down",
-                padding: 4,
-            },
-        }),
-        new SpritesmithPlugin({
-            // 目标小图标
-            src: {
-                //下面的路径，根据自己的实际路径配置
-                cwd: path.resolve(__dirname, "../src/assets/flags"),
-                glob: "*.png",
-            },
-            // 输出雪碧图文件及样式文件
-            target: {
-                //下面的路径，根据自己的实际路径配置
-                image: path.resolve(__dirname, "../public/sprites/flags.png"),
-                css: path.resolve(__dirname, "../public/sprites/flags.css"),
-            },
-            // 样式文件中调用雪碧图地址写法
-            apiOptions: {
-                // 这个路径根据自己页面配置
-                cssImageRef: "./flags.png",
-            },
-            spritesmithOptions: {
-                algorithm: "top-down",
-                padding: 4,
-            },
-        }),
+          },
+        ],
+      },
     ],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "../src"),
-        },
-        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+  },
+  plugins: [
+    new AntdDayjsWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "创客",
+      filename: path.resolve(__dirname, "../dist/index.html"),
+      template: path.resolve(__dirname, "../public/index.html"),
+    }),
+    new SpritesmithPlugin({
+      // 目标小图标
+      src: {
+        //下面的路径，根据自己的实际路径配置
+        cwd: path.resolve(__dirname, "../src/assets/icons"),
+        glob: "*.png",
+      },
+      // 输出雪碧图文件及样式文件
+      target: {
+        //下面的路径，根据自己的实际路径配置
+        image: path.resolve(__dirname, "../public/sprites/sprite.png"),
+        css: path.resolve(__dirname, "../public/sprites/sprite.css"),
+      },
+      // 样式文件中调用雪碧图地址写法
+      apiOptions: {
+        // 这个路径根据自己页面配置
+        cssImageRef: "./sprite.png",
+      },
+      spritesmithOptions: {
+        algorithm: "top-down",
+        padding: 4,
+      },
+    }),
+    new SpritesmithPlugin({
+      // 目标小图标
+      src: {
+        //下面的路径，根据自己的实际路径配置
+        cwd: path.resolve(__dirname, "../src/assets/flags"),
+        glob: "*.png",
+      },
+      // 输出雪碧图文件及样式文件
+      target: {
+        //下面的路径，根据自己的实际路径配置
+        image: path.resolve(__dirname, "../public/sprites/flags.png"),
+        css: path.resolve(__dirname, "../public/sprites/flags.css"),
+      },
+      // 样式文件中调用雪碧图地址写法
+      apiOptions: {
+        // 这个路径根据自己页面配置
+        cssImageRef: "./flags.png",
+      },
+      spritesmithOptions: {
+        algorithm: "top-down",
+        padding: 4,
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "../src"),
     },
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+  },
 };
--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -5,75 +5,75 @@ const base = require("./webpack.base.config");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = merge(base, {
-    mode: "development",
-    devtool: "source-map",
-    output: {
-        path: path.resolve(__dirname, "../dist"),
-        filename: "js/[name].js",
-        publicPath: "/",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [{
-                    loader: "style-loader",
-                }, {
-                    loader: "css-loader",
-                    options: {
-                        esModule: false
-                    }
-                }, {
-                    loader: "postcss-loader"
-                }],
+  mode: "development",
+  devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "js/[name].js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{
+          loader: "style-loader",
+        }, {
+          loader: "css-loader",
+          options: {
+            esModule: false
+          }
+        }, {
+          loader: "postcss-loader"
+        }],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              esModule: false
+            }
+          },
+          {
+            loader: "postcss-loader"
+          },
+          {
+            loader: "less-loader",
+            options: {
+              javascriptEnabled: true,
             },
-            {
-                test: /\.less$/,
-                use: [
-                    {
-                        loader: "style-loader",
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            esModule: false
-                        }
-                    },
-                    {
-                        loader: "postcss-loader"
-                    },
-                    {
-                        loader: "less-loader",
-                        options: {
-                            javascriptEnabled: true,
-                        },
-                    },
-                ],
-            },
+          },
         ],
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            analyzerPort: 8881,
-        }),
+      },
     ],
-    devServer: {
-        port: "9000",
-        host: "localhost",
-        allowedHosts: ["0.0.0.0", "192.168.1.49"],
-        open: true,
-        hot: true,
-        historyApiFallback: true,
-        contentBase: path.join(__dirname, "../dist"),
-        proxy: {
-            "/api": {
-                target: "http://localhost:3002",
-                changeOrigin: true,
-                // pathRewrite: {
-                //     "/api": ""
-                // },
-            },
-        },
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerPort: 8881,
+    }),
+  ],
+  devServer: {
+    port: "9000",
+    host: "localhost",
+    allowedHosts: ["0.0.0.0", "192.168.1.49"],
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, "../dist"),
+    proxy: {
+      "/api": {
+        target: "http://localhost:3002",
+        changeOrigin: true,
+        // pathRewrite: {
+        //     "/api": ""
+        // },
+      },
     },
+  },
 });
