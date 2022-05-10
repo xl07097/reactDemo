6a22903f9a9ebd722f97698ca2a5a297d1969d61
xueliang <1058392650@qq.com>
2022-05-10 09:34:11 +0800

    format

--- src/views/context/Child2.jsx
+++ src/views/context/Child2.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { context } from "./context";
 
 export default class Child2 extends React.Component {
-    render() {
-        return (
-            <div style={{padding:'0 10px'}}>
-                <span style={{ wordBreak: "break-all" }}>{this.context}</span>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div style={{ padding: "0 10px" }}>
+        <span style={{ wordBreak: "break-all" }}>{this.context}</span>
+      </div>
+    );
+  }
 }
 Child2.contextType = context;
